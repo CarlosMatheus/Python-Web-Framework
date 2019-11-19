9686daa04ff1d325cf20c508db3e5fdb36b35e48
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-11-19 02:35:07 -0300

    Rename file

--- app_test.py
+++ app_test.py
@@ -5,6 +5,8 @@ This exemplify how a WSGI works
 # This is a function from a default python library that allow us to make a WSGI server.
 from wsgiref.simple_server import make_server
 
+from wsgi.web_server_gateway_interface import WebServerGatewayInterface
+
 
 def web_app(environment, response):
     """
@@ -18,14 +20,26 @@ def web_app(environment, response):
     headers = [('Content-type', 'text/html; charset=utf-8')]
     response(status, headers)
 
-    return [b'<strong> Hello World I just created my first WSGI</strong>']
+    f = open('./templates/index.html', 'r').read()
+
+    return [f.encode()]
 
 
 # Empty string means that the host will be local host
 host = ''
 port = 8080
 
-with make_server(host, port, web_app) as server:
+# with make_server(host, port, web_app) as server:
+#     if not host:
+#         print("Serving on port %d \nVisit http://127.0.0.1:%d" % (port, port))
+#     else:
+#         print("Serving on port %d \nVisit http://%s:%d" % (port, host, port))
+#     print("To kill the server enter 'control + c'")
+#
+#     # will server forever until we kill it
+#     server.serve_forever()
+
+with WebServerGatewayInterface(host, port, web_app) as server:
     if not host:
         print("Serving on port %d \nVisit http://127.0.0.1:%d" % (port, port))
     else:
@@ -34,5 +48,4 @@ with make_server(host, port, web_app) as server:
 
     # will server forever until we kill it
     server.serve_forever()
-
 
